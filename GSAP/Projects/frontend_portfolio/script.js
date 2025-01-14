51b9b939bdc76ac64f39fa3a41d876e074928b29
GOPAL SHUKLA <89374640+Gopalshukla0018@users.noreply.github.com>
2025-01-14 20:12:45 +0000

    Pending changes exported from your codespace

---
+++ GSAP/Projects/frontend_portfolio/script.js
@@ -0,0 +1,17 @@
+var tl = gsap.timeline()
+
+tl.from("#nav h3",{
+    y:-100,
+    delay:     0.3,
+    duration: 0.8,
+   stagger:0.4,
+   opacity:0,
+
+})
+
+tl.from("#main h1",{
+    x:-500,
+    duration:0.8,
+    opacity:0,
+    stagger:0.4
+})

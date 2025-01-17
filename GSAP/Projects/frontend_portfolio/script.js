940bf9a977e8e0fe0d64357d01fe610edaa78ec0
GOPAL SHUKLA <89374640+Gopalshukla0018@users.noreply.github.com>
2025-01-17 07:32:01 +0000

    gsap

--- GSAP/Projects/frontend_portfolio/script.js
+++ GSAP/Projects/frontend_portfolio/script.js
@@ -13,5 +13,17 @@ tl.from("#main h1",{
     x:-500,
     duration:0.8,
     opacity:0,
-    stagger:0.4
+    stagger:0.4,
+    
+})
+
+tl.from("img",{
+    x:100,
+    rotate:30,
+    opacity:0,
+    duration:0.8,
+    
+    
+    stagger:0.8
+    
 })

f0da2f3d92971f39abddb5bcefb29905959f4ce7
Gopal Shukla <gopalshukla0018@gmail.com>
2025-02-06 17:39:56 +0530

    to be contined.. Tic Tac Toe

--- Projects/Tik Tac Toe/app.js
+++ Projects/Tik Tac Toe/app.js
@@ -37,13 +37,16 @@ boxes.forEach((box) => {
 
 const checkwinner =() =>{
 
-    for (const pattern of winPatterns) {
-        console.log(pattern[0],pattern[1],pattern[2]);
+    for (let pattern of winPatterns) {
+        console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
+        console.console.log(pattern[0],pattern[1],pattern[2]);
+        console.log(
+            boxes[pattern[0]].innerText,
+            boxes[pattern[1]].innerText,
+            boxes[pattern[2]].innerText
+        );
+        // to be continue......
 
-
-
-
-        
 
         
         

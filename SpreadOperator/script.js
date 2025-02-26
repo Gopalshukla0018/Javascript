ec8c4f78992ce2362ddcb2003e280f7cf086e83e
Gopal Shukla <gopalshukla0018@gmail.com>
2025-02-26 12:02:28 +0530

    add comment in spredOperator

--- SpreadOperator/script.js
+++ SpreadOperator/script.js
@@ -1,25 +1,32 @@
-const nums1 = [1,2,3,4,5]
-const nums2 = [6,7,8,9]
+// Creating two arrays
+const nums1 = [1, 2, 3, 4, 5];
+const nums2 = [6, 7, 8, 9];
 
-const joinedArray = [...nums1,...nums2]
+// Merging both arrays using spread operator
+const joinedArray = [...nums1, ...nums2];
 
 
+// Creating a user object
 const user = {
     name: 'Gopal',
     age: 20,
-}
+};
 
-const updatedUser ={
-    ...user,
-    city:'Noida'
+// Creating a new object by copying 'user' and adding a new property 'city'
+const updatedUser = {
+    ...user,  // Copying existing properties from 'user'
+    city: 'Noida'  // Adding a new property
+};
 
-}
 
-function add(){
-    let sum=0
-    for(let i=0;i<arguments.length;i++){
-        sum = sum+(arguments[i]);
-        
+// Function to add multiple numbers
+function add() {
+    let sum = 0;
+
+    // Looping through all arguments (arguments is an array-like object)
+    for (let i = 0; i < arguments.length; i++) {
+        sum = sum + arguments[i]; // Adding each argument to 'sum'
     }
-    return sum;
+
+    return sum; // Returning the total sum
 }

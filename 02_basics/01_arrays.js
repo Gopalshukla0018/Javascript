65c03a4e9acddae1bffdf14ad7c7f4ad26c67ea5
Gopal Shukla <gopalshukla0018@gmail.com>
2025-03-17 17:25:51 +0530

    solve array questions

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,42 +1,70 @@
-// array
+// // array
 
-const myArr = [ 1, 23 , 23 , 45 , 7]
-const myHeros = ["shaktiman","naagraj"]
+// const myArr = [ 1, 23 , 23 , 45 , 7]
+// const myHeros = ["shaktiman","naagraj"]
 
-const myArr2 = new Array(1,2,3,4)
-console.log(myArr[1])
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1])
 
-console.log(myArr2.length);
+// console.log(myArr2.length);
 
-// Array methods
+// // Array methods
 
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
-// myArr.cancat() -- array ko jod deta hai--
+// // myArr.push(6)
+// // myArr.push(7)
+// // myArr.pop()
+// // myArr.cancat() -- array ko jod deta hai--
 
-// myArr.unshift(9)
-// myArr.shift()
+// // myArr.unshift(9)
+// // myArr.shift()
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// // console.log(myArr.includes(9));
+// // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+// // const newArr = myArr.join()
 
-// console.log(myArr);
-// console.log( newArr);
+// // console.log(myArr);
+// // console.log( newArr);
+
+// // slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
+
+// how to compare
+
+// const fruits=["Banana","Apple","Apple","Orange","Mango"];
+
+// const number = [1, 2, 4, 3, 8, 5, 6, 7,9];
+// number.sort();
+// console.log(number);
+
+// compare callback function
+// const number = [1, 2, 4, 3, 8, 5, 6, 7,9];
+// number.sort(a,b)
+
+//! 1: Using the map method, write a function that takes an array of strings and returns a new array where each string is capitalized.
+// Original array of strings
+// const words = ["APPLE", "banana", "cherry", "date"];
+// const result = words.map((curElem) => {
+//   return curElem.toUpperCase();
+// });
+// console.log(result);
+
+// lower case
+
+// const words = ["APPLE", "banana", "cherry", "date"];
+// const result = words.map((curElem) => {
+//   return curElem.toLowerCase();
+// });
+// console.log(result);
 
 
-// slice, splice
-
-console.log("A ", myArr);
-
-const myn1 = myArr.slice(1, 3)
-
-console.log(myn1);
-console.log("B ", myArr);
-
-
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
---
+++ 02_basics/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+const words = ["APPLE", "banana", "cherry", "date"];
+// const result = words.map((curElem) => {
+//   return curElem.toUpperCase();
+// });
+// console.log(result);
